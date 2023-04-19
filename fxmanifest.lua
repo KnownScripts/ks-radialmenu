5c720cef24660c1923111e109b0d011efd61d591
Known Scripts <70570844+KnownScripts@users.noreply.github.com>
2023-04-20 01:20:31 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,24 +10,24 @@ description 'An radial options for qbus'
 
 lua54 'yes'
 client_scripts {
-	"@PolyZone/client.lua",
-	"@PolyZone/BoxZone.lua",
-	"@PolyZone/EntityZone.lua",
-	"@PolyZone/CircleZone.lua",
-	"@PolyZone/ComboZone.lua",
+    "@PolyZone/client.lua",
+    "@PolyZone/BoxZone.lua",
+    "@PolyZone/EntityZone.lua",
+    "@PolyZone/CircleZone.lua",
+    "@PolyZone/ComboZone.lua",
     "config.lua",
     "client_menu.lua",
-	"utils.lua",
-	"client/stretcher.lua",
-	"client/clothing.lua",
-	"client/trunk.lua",
-	"emotes_triggers.lua",
+    "utils.lua",
+    "client/stretcher.lua",
+    "client/clothing.lua",
+    "client/trunk.lua",
+    "emotes_triggers.lua",
 }
 
 server_scripts {
-	"server/stretcher.lua",
-	"server/trunk.lua",
-	"server/main.lua",
+   "server/stretcher.lua",
+   "server/trunk.lua",
+   "server/main.lua",
 }
 
 shared_script "@ks-garaaz/config.lua"
@@ -36,10 +36,10 @@ shared_script "@Framework/imports.lua"
 
 ui_page "html/ui.html"
 files {
-	"html/ui.html",
-	"html/css/RadialMenu.css",
-	"html/js/RadialMenu.js",
-	'html/css/all.min.css',
-	'html/js/all.min.js',
+   "html/ui.html",
+   "html/css/RadialMenu.css",
+   "html/js/RadialMenu.js",
+   'html/css/all.min.css',
+   'html/js/all.min.js',
 	
 }
